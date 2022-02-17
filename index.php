f0cd47c4f01c5e29db31d29602472d5bff222c0c
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-02-16 16:49:22 -0800

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,9 @@
         <div class="vertical-center">
           <h2>Skills</h2>
             <h4><?php echo $json_data['skills']; ?><h4>
-          <p>...</p>
+                <p>"System Administration"</p>
+                <p>"Technical Skills: Configure Networks, Network Security, Troubleshooting, Software Installation and Management"</p>
+                <p>"Programming Languages: C++, PuTTY, Python, HTML, CSS"</p>
         </div>
       </div>
       <hr>
@@ -50,7 +52,9 @@
         <div class="vertical-center">
           <h2>Interests</h2>
             <h4><?php echo $json_data['interests']; ?><h4>
-            <p>...</p>
+                <p>"extended knowledge in computer languages"</p>
+                <p>"web development"</p>
+                <p>"extended knowledge in System Administration"</p>
         </div>
       </div>
       <hr>
